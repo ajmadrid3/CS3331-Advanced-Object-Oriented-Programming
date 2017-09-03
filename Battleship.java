fb417c7dc7addd84aa7259271791868439452804
Andrew Madrid <ajmadrid3@miners.utep.edu>
2017-09-03 12:55:27 -0600

    Added Input method to account for multiple types of input.

--- Battleship.java
+++ Battleship.java
@@ -1,6 +1,7 @@
 /*
  * Andrew Madrid
  * CS 3331 - Advanced Object-Oriented Programming
+ * Assignment 1 - Battle Ship Game
  * Fall 2017
  * Purpose:
  * 	This program allows users to enter a point on the board.  It will then list all of the
@@ -11,16 +12,125 @@ public class Battleship
 {	
 	public static boolean board[][] = new boolean[9][9];
 	
-	//Used to find and print the squares that are edge adjacent to the given point.
-	public static void edge_Adjacency(int x_coordinate, int y_coordinate)
+	//Used to determine the type of input the user enters and if they entered valid coordinates.
+	public static int[] input_Reader()
 	{
-		System.out.println("Squares Edge Adjacenct to (" + x_coordinate + ", " + y_coordinate + "):");
+		Scanner scnr = new Scanner(System.in);
+		boolean valid_format = false;
+		boolean valid_choice = false;
+		int[] coordinates = new int[2];
+		
+		while(!valid_choice)
+		{
+			System.out.println("Enter the coordinates (0 to 8 or a to i) in one of the following formats: ");
+			System.out.println("(5, 5)   3 0   4,1   a7   b 2");
+			System.out.print("Entered Coordinates: ");
+			String input = scnr.nextLine();
+			
+			// Checks to see if the parenthesis format is entered correctly
+			if(input.charAt(0) == '(')
+			{
+				if(!Character.isDigit(input.charAt(1)) || !Character.isDigit(input.charAt(4)))
+				{
+					System.out.println("ERROR: Invalid Format\n");
+				}
+				else
+				{
+					coordinates[0] = Character.getNumericValue(input.charAt(1));
+					coordinates[1] = Character.getNumericValue(input.charAt(4));
+					valid_format = true;
+				}
+			}
+			
+			// Checks to see if either of the letter formats are entered correctly
+			else if(Character.isLetter(input.charAt(0)))
+			{
+				input.toLowerCase();
+				char value = input.charAt(0);
+				
+				switch (value)
+				{
+					case 'a': coordinates[0] = 0;
+						break;
+					case 'b': coordinates[0] = 1;
+						break;
+					case 'c': coordinates[0] = 2;
+						break;
+					case 'd': coordinates[0] = 3;
+						break;
+					case 'e': coordinates[0] = 4;
+						break;
+					case 'f': coordinates[0] = 5;
+						break;	
+					case 'g': coordinates[0] = 6;
+						break;
+					case 'h': coordinates[0] = 7;
+						break;
+					case 'i': coordinates[0] = 8;
+						break;
+					default: System.out.println("ERROR: Invalid Value Entered\n");
+				}
+				
+				if(Character.isDigit(input.charAt(1)))
+				{
+					coordinates[1] = Character.getNumericValue(input.charAt(1));
+					valid_format = true;
+				}
+				else if(Character.isDigit(input.charAt(2)))
+				{
+					coordinates[1] = Character.getNumericValue(input.charAt(2));
+					valid_format = true;
+				}
+				else
+					System.out.println("ERROR: Invalid Format\n");
+			}
+			
+			// Checks to see if either of the digit formats are entered correctly
+			else if(Character.isDigit(input.charAt(0)))
+			{
+				coordinates[0] = Character.getNumericValue(input.charAt(0));
+				if(input.charAt(1) == ',' || input.charAt(1) == ' ')
+				{
+					if(Character.isDigit(input.charAt(2)))
+					{
+						coordinates[1] = Character.getNumericValue(input.charAt(2));
+						valid_format = true;
+					}
+					else
+						System.out.println("ERROR: Invalid Format\n");
+				}
+				else
+					System.out.println("ERROR: Invalid Format\n");
+			}
+			
+			// Gives the error that an unaccepted format was used
+			else
+				System.out.println("ERROR: Invalid Format\n");
+			
+			// If a valid format was entered, checks to see if the values are between 0 and 8
+			if(valid_format) 
+			{
+				if((coordinates[0] >= 0 && coordinates[0] < 9) || (coordinates[1] >= 0 && coordinates[1] < 9))
+					valid_choice = true;
+				else
+					System.out.println("ERROR: Invalid Values Entered");
+			}
+		}
+		System.out.println("Coordinates Accecpted\n");
+		board[coordinates[0]][coordinates[1]] = true;
+		return coordinates;
+	}
+	
+	//Used to find and print the squares that are edge adjacent to the given point.
+	public static void edge_Adjacency(int[] coordinates)
+	{
+		System.out.println("Squares Edge Adjacenct to (" + coordinates[0] + ", " + coordinates[1] + "):");
 		
 		int[][] adjacent = {
-							{x_coordinate - 1, y_coordinate},	// Left
-							{x_coordinate + 1, y_coordinate},	// Right
-							{x_coordinate, y_coordinate - 1},	// Upper
-							{x_coordinate, y_coordinate + 1}	// Lower
+							{coordinates[0] - 1, coordinates[1]},	// Left
+							{coordinates[0] + 1, coordinates[1]},	// Right
+							{coordinates[0], coordinates[1] - 1},	// Upper
+							{coordinates[0], coordinates[1] + 1}	// Lower
 						   };
 		
 		for(int counter = 0; counter < adjacent.length; counter ++)
@@ -34,15 +144,15 @@ public class Battleship
 	}
 	
 	//Used to find and print the squares that are corner adjacent to the given point. 
-	public static void corner_Adjacency(int x_coordinate, int y_coordinate)
+	public static void corner_Adjacency(int[] coordinates)
 	{
-		System.out.println("Squares Corner Adjacent to (" + x_coordinate + ", " + y_coordinate + "):");
+		System.out.println("Squares Corner Adjacent to (" + coordinates[0] + ", " + coordinates[1] + "):");
 		
 		int[][] adjacent = {
-							{x_coordinate - 1, y_coordinate - 1},	// Bottom Left
-							{x_coordinate + 1, y_coordinate - 1},	// Bottom Right	
-							{x_coordinate - 1, y_coordinate + 1},	// Upper Left
-							{x_coordinate + 1, y_coordinate + 1}	// Upper Right
+							{coordinates[0] - 1, coordinates[1] - 1},	// Bottom Left
+							{coordinates[0] + 1, coordinates[1] - 1},	// Bottom Right	
+							{coordinates[0] - 1, coordinates[1] + 1},	// Upper Left
+							{coordinates[0] + 1, coordinates[1] + 1}	// Upper Right
 							};
 		
 		for(int counter = 0; counter < adjacent.length; counter ++)
@@ -56,9 +166,9 @@ public class Battleship
 	}
 	
 	// Prints out all of the squares that are not adjacent to the given point.
-	public static void non_Adjacency(int x_coordinate, int y_coordinate)
+	public static void non_Adjacency(int[] coordinates)
 	{
-		System.out.println("Squares Not Adjacent to (" + x_coordinate + ", " + y_coordinate + "):");
+		System.out.println("Squares Not Adjacent to (" + coordinates[0] + ", " + coordinates[1] + "):");
 		for(int i = 0; i < board.length; i++)
 		{
 			for(int j = 0; j < board[i].length; j++)
@@ -73,42 +183,12 @@ public class Battleship
 	
 	public static void main(String[] args) 
 	{	
-		Scanner scnr = new Scanner(System.in);
-		boolean valid_choice = false;
-		int x_coordinate = 0, y_coordinate = 0;
-		while(!valid_choice)
-		{
-			System.out.print("Enter the x-coordinate of the square: ");
-			x_coordinate = scnr.nextInt();
-			if(x_coordinate < 0 || x_coordinate > 8)
-			{
-				System.out.println("ERROR: Invalid x-coordinate.");
-			}
-			else
-			{
-				valid_choice = true;
-			}
-		}
+		System.out.println("Battleship Square Adjacency\n");
 		
-		valid_choice = false;
-		while(!valid_choice)
-		{
-			System.out.print("Enter the y-coordinate of the square: ");
-			y_coordinate = scnr.nextInt();
-			if(y_coordinate < 0 || y_coordinate > 8)
-			{
-				System.out.println("ERROR: Invalid y-coordinate.");
-			}
-			else
-			{
-				valid_choice = true;
-			}
-		}
-		board[x_coordinate][y_coordinate] = true;
-		
-		edge_Adjacency(x_coordinate, y_coordinate);
-		corner_Adjacency(x_coordinate, y_coordinate);
-		non_Adjacency(x_coordinate, y_coordinate);
+		int[] coordinates = input_Reader();
+		edge_Adjacency(coordinates);
+		corner_Adjacency(coordinates);
+		non_Adjacency(coordinates);
 	}
 
 }
